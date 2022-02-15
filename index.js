0737887047c3b85489ed5f4b188f36d478eaead0
salman-abdoh <80646924+salman-abdoh@users.noreply.github.com>
2022-02-15 04:17:17 +0300

    creatbranch

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const mongoose=require('mongoose')
+const http =require('http');
+const fs =require('fs');
+const path =require('path');
+
+const hostname ='127.0.0.1';
+const port =5000;
+const server = http.createServer((request,response)    => {
+    response.writeHead( 200, {
+'content-type':'text/html'
+    });
+    let url=request.url;
+  
+    if (url === '/'){
+        fs.readFile(path.join(__dirname,'index.html'),(err,data)=>{
+            if(err) throw err;
+            response.end(data);  });
+    }
+ else if(url==='/about'){
+    fs.readFile(path.join(__dirname,'about.html'),(err,data)=>{
+        if(err) throw err;
+        response.end(data);  });
+ }
+ else if(url==='/login'){
+    fs.readFile(path.join(__dirname,'login.html'),(err,data)=>{
+    
+   
+   
+     if(err) throw err;
+    response.end(data);  });}
+   
+ else {
+    fs.readFile(path.join(__dirname,'erorr.html'),(err,data)=>{
+        if(err) throw err;
+        response.end(data);  });
+ }
 
 
+});
+server.listen(port,hostname,()=>{
+console.log('http://${hostname}:${port}');
+}); 
